208cefc0bcdfcbd23673079de63c7c87ba01c964
Dvam leite <dvanlds@gmail.com>
2023-03-27 20:37:13 -0300

    update style function

--- js/script.js
+++ js/script.js
@@ -23,3 +23,15 @@ window.onscroll = () =>{
         };
     });
 }; 
+
+/*=================scroll=====================*/
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .servicos-container, .portfolio-box, .contato form', {origin: 'top'});
